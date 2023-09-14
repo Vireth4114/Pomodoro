a93a40e0c86d83c1c938459f9ed2ffe591855153
aubry224 <aubry224@unicaen.fr>
2023-09-14 20:38:43 +0200

    Finished basic Pomodoro implementation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 let minutes = 25;
 let seconds = 0;
-
 let timerSetInterval;
+let isPause = false;
+
+let minutesElement = document.getElementById("minutes");
+let secondsElement = document.getElementById("seconds");
+let workElement = document.getElementById("work");
+let pauseElement = document.getElementById("pause");
+
 
 function displayTime(time) {
     let timeString = time.toString();
@@ -9,8 +15,26 @@ function displayTime(time) {
     return formattedString;
 }
 
-document.getElementById("minutes").innerHTML = displayTime(minutes);
-document.getElementById("seconds").innerHTML = displayTime(seconds);
+function updateTimer() {
+    minutesElement.innerHTML = displayTime(minutes);
+    secondsElement.innerHTML = displayTime(seconds);
+}
+
+function updateMode() {
+    if (isPause) {
+        workElement.style.background = "#102030";
+        pauseElement.style.background = "#081018";
+        workElement.style.color = "#606060";
+        pauseElement.style.color = "white";
+    } else {
+        workElement.style.background = "#081018";
+        pauseElement.style.background = "#102030";
+        workElement.style.color = "white";
+        pauseElement.style.color = "#606060";
+    }
+}
+
+updateTimer();
 
 function timerRunning() {
     seconds--;
@@ -18,10 +42,13 @@ function timerRunning() {
         minutes--;
         seconds = 59;
     }
-    document.getElementById("minutes").innerHTML = displayTime(minutes)
-    document.getElementById("seconds").innerHTML = displayTime(seconds)
+    updateTimer();
     if (minutes == 0 && seconds == 0) {
-        document.getElementById("timerCircle").innerHTML = "<i class='fa-solid fa-clock fa-shake'></i>";
+        minutes = isPause ? 25 : 1;
+        seconds = 0;
+        updateTimer();
+        isPause = !isPause;
+        updateMode();
     }
 }
 
@@ -32,13 +59,18 @@ startButton.onclick = function() {
     timerSetInterval = setInterval(timerRunning, 1000);resetButton.style.display = "none";
     resetButton.style.display = "initial";
     startButton.replaceWith(resetButton);
+    isPause = false;
+    updateMode();
 };
 
 resetButton.onclick = function() {
     minutes = 25;
     seconds = 0;
-    document.getElementById("minutes").innerHTML = displayTime(minutes);
-    document.getElementById("seconds").innerHTML = displayTime(seconds);
+    updateTimer();
     resetButton.replaceWith(startButton);
     clearInterval(timerSetInterval);
+    workElement.style.background = "#102030";
+    pauseElement.style.background = "#102030";
+    workElement.style.color = "#606060";
+    pauseElement.style.color = "#606060";
 }
